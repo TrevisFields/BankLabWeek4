107ffd88cb3913f90e3646b99dec75ae5d429465
Trevis Jontaé Fields <tfields3@student.rcc.edu>
2017-03-06 10:17:37 -0800

    added bank, account, and customer files
    added bank header and cpp, account header and cpp, and customer header
    and cpp files

---
+++ BankExample/BankExample/Bank.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+#include "Account.h"
+class Bank
+{
+private:
+  std::string _name;
+  std::vector<Account> _accounts;
+  
+public:
+  Bank(std::string name);
+  ~Bank();
+  
+  std::string getName();
+};
